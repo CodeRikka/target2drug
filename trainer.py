ddb4ab9e867f0de6d8f4f9e0ebf5b865ab0b92b0
coderikka <2293624891@qq.com>
2023-12-06 14:16:23 +0800

    bad commit

--- trainer.py
+++ trainer.py
@@ -23,11 +23,11 @@ num_layers = 8
 
 model = VQVAE(dataset.tokenizer, input_dim, hidden_dim, num_embeddings, embedding_dim, num_heads, num_layers)
 
-loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True, collate_fn=dataset.collate_fn)
+loader = DataLoader(dataset=dataset, batch_size=2, shuffle=True, collate_fn=dataset.collate_fn)
 
 for step, batch_data in tqdm(enumerate(loader), total=len(loader)):
 
-    protein_emmbedding, corrupted_inputs, input_mask, target_seqs = [i.to('cuda:0') for i in batch_data]
+    protein_emmbeddings, corrupted_inputs, input_mask, target_seqs = [i.to('cuda:0') for i in batch_data]
         
     # print(protein_emmbedding)
     # print(corrupted_inputs)
@@ -37,6 +37,14 @@ for step, batch_data in tqdm(enumerate(loader), total=len(loader)):
         # print(protein_emmbedding, corrupted_inputs, input_mask, target_seqs)
     batch_size = corrupted_inputs.shape[0]
     
+    prediction_scores, lm_loss, vq_loss = model(protein_emmbeddings, corrupted_inputs, input_mask, target_seqs)
+    
+    # NG
+    beta = 0.2
+    loss = lm_loss + vq_loss * beta
+    
+    
+    loss.backward()
     # model(protein_emmbedding, corrupted_inputs, input_mask, target_seqs)
     
     
